059ef137f696b227c2e7c0e551063f50f03c54b7
Michael Charara <66533339+Michael-Charara@users.noreply.github.com>
2021-06-04 23:11:24 -0400

    PA1 update

--- Programming_Assignment_1.cpp
+++ Programming_Assignment_1.cpp
@@ -1,6 +1,5 @@
 /*
- *
- * Three employees in a company are up for a special pay increase. You are
+Three employees in a company are up for a special pay increase. You are
 given a file, say Ch3_Ex7Data.txt , with the following data:
 Miller Andrew 65789.87 5
 Green Sheila 75892.56 6
@@ -13,7 +12,7 @@ the specified file and stores the output in the file Ch3_Ex7Output.dat .
 For each employee, the data must be output in the following form:
 firstName lastName updatedSalary . Format the output of decimal
 numbers to two decimal places.
- */
+*/
 
 #include <iostream>
 #include <fstream>
@@ -23,21 +22,25 @@ using namespace std;
 
 int main()
 {
+    // First employee (employee A) variable declaration
     string lastnameA, firstnameA;
     double currentSalaryA, percentPayIncreaseA, updatedSalaryA;
 
+    // Second employee (employee B) variable declaration
     string lastnameB, firstnameB;
     double currentSalaryB, percentPayIncreaseB, updatedSalaryB;
 
+    // Third employee (employee C) variable declaration
     string lastnameC, firstnameC;
     double currentSalaryC, percentPayIncreaseC, updatedSalaryC;
 
-    ifstream inFile;
-    ofstream outFile;
-    inFile.open("Ch3_Ex7Data.txt");
-    outFile.open("Ch3_Ex7Output.dat");
+    ifstream inFile;                   // Declare input variable for file
+    ofstream outFile;                  // Declare output variable for file
+    inFile.open("Ch3_Ex7Data.txt");    // open input file
+    outFile.open("Ch3_Ex7Output.dat"); // create and or open output file
     outFile << setprecision(2) << showpoint << fixed;
 
+    // Employee processing for A B and C
     inFile >> lastnameA >> firstnameA >> currentSalaryA >>
         percentPayIncreaseA;
 
@@ -47,6 +50,7 @@ int main()
     inFile >> lastnameC >> firstnameC >> currentSalaryC >>
         percentPayIncreaseC;
 
+    // calculating updated salaries for employees A B and C
     updatedSalaryA = currentSalaryA + (currentSalaryA * percentPayIncreaseA / 100);
     updatedSalaryB = currentSalaryB + (currentSalaryB * percentPayIncreaseB / 100);
     updatedSalaryC = currentSalaryC + (currentSalaryC * percentPayIncreaseC / 100);
@@ -55,6 +59,7 @@ int main()
     outFile << lastnameB << " " << firstnameB << " " << updatedSalaryB << endl;
     outFile << lastnameC << " " << firstnameC << " " << updatedSalaryC << endl;
 
+    // close input and output file
     inFile.close();
     outFile.close();
     return 0;
